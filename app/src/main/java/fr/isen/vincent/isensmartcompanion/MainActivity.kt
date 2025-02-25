6c2fb3ca4140999811b9550415edae27d5ff0318
DramatikzZ <clevin13@gmail.com>
2025-02-25 12:57:51 +0100

    PART 2 - 1

--- app/src/main/java/fr/isen/vincent/isensmartcompanion/MainActivity.kt
+++ app/src/main/java/fr/isen/vincent/isensmartcompanion/MainActivity.kt
@@ -5,21 +5,63 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.DateRange
+import androidx.compose.material.icons.filled.Home
+import androidx.compose.material.icons.filled.List
+import androidx.compose.material.icons.outlined.DateRange
+import androidx.compose.material.icons.outlined.Home
+import androidx.compose.material.icons.outlined.List
+import androidx.compose.material.icons.outlined.Notifications
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.tooling.preview.Preview
-import fr.isen.vincent.isensmartcompanion.screen.MainScreen
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
 import fr.isen.vincent.isensmartcompanion.ui.theme.ISENSmartCompanionTheme
+
+import fr.isen.vincent.isensmartcompanion.screen.MainScreen
+import fr.isen.vincent.isensmartcompanion.screen.TabView
+
+
+data class TabBarItem(
+    val title: String,
+    val selectedIcon: ImageVector,
+    val unselectedIcon: ImageVector,
+    val badgeAmount: Int? = null
+)
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
+            val homeTab = TabBarItem(title = "Home", selectedIcon = Icons.Filled.Home, unselectedIcon = Icons.Outlined.Home)
+            val eventsTab = TabBarItem(title = "Events", selectedIcon = Icons.Filled.DateRange, unselectedIcon = Icons.Outlined.DateRange, /*badgeAmount = 7*/)
+            val historyTab = TabBarItem(title = "History", selectedIcon = Icons.Filled.List, unselectedIcon = Icons.Outlined.List)
+
+            // creating a list of all the tabs
+            val tabBarItems = listOf(homeTab, eventsTab, historyTab)
+
+            // creating our navController
+            val navController = rememberNavController()
+
             ISENSmartCompanionTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    MainScreen(innerPadding)
+                Scaffold(bottomBar = {TabView(tabBarItems, navController)}, modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    NavHost(navController = navController, startDestination = homeTab.title){
+                        composable(homeTab.title){
+                            MainScreen(innerPadding)
+                        }
+                        composable(eventsTab.title){
+                        }
+                        composable(historyTab.title){
+                            Text(historyTab.title)
+                        }
+                    }
                 }
             }
         }
@@ -27,10 +69,11 @@ class MainActivity : ComponentActivity() {
 }
 
 
+
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
     ISENSmartCompanionTheme {
-        // Mettre des fonctions test
+        //MainScreen(PaddingValues)
     }
 }
