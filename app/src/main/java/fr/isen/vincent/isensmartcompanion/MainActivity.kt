65130bd353777582c95ac9ffba43c9ac9ed221a3
DramatikzZ <clevin13@gmail.com>
2025-02-25 15:21:09 +0100

    PART 2 - 2

--- app/src/main/java/fr/isen/vincent/isensmartcompanion/MainActivity.kt
+++ app/src/main/java/fr/isen/vincent/isensmartcompanion/MainActivity.kt
@@ -1,6 +1,8 @@
 package fr.isen.vincent.isensmartcompanion
 
+import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
@@ -12,20 +14,22 @@ import androidx.compose.material.icons.filled.List
 import androidx.compose.material.icons.outlined.DateRange
 import androidx.compose.material.icons.outlined.Home
 import androidx.compose.material.icons.outlined.List
-import androidx.compose.material.icons.outlined.Notifications
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.core.content.ContextCompat.startActivity
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
-import fr.isen.vincent.isensmartcompanion.ui.theme.ISENSmartCompanionTheme
-
+import fr.isen.vincent.isensmartcompanion.models.EventModel
+import fr.isen.vincent.isensmartcompanion.screen.EventsScreen
+import fr.isen.vincent.isensmartcompanion.screen.HistoryScreen
 import fr.isen.vincent.isensmartcompanion.screen.MainScreen
 import fr.isen.vincent.isensmartcompanion.screen.TabView
+import fr.isen.vincent.isensmartcompanion.ui.theme.ISENSmartCompanionTheme
+
 
 
 data class TabBarItem(
@@ -35,14 +39,16 @@ data class TabBarItem(
     val badgeAmount: Int? = null
 )
 
+
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        Log.d("lifecycle", "MainActivity onCreate")
         enableEdgeToEdge()
         setContent {
-            val homeTab = TabBarItem(title = "Home", selectedIcon = Icons.Filled.Home, unselectedIcon = Icons.Outlined.Home)
-            val eventsTab = TabBarItem(title = "Events", selectedIcon = Icons.Filled.DateRange, unselectedIcon = Icons.Outlined.DateRange, /*badgeAmount = 7*/)
-            val historyTab = TabBarItem(title = "History", selectedIcon = Icons.Filled.List, unselectedIcon = Icons.Outlined.List)
+            val homeTab = TabBarItem(title = getString(R.string.bottom_navbar_home), selectedIcon = Icons.Filled.Home, unselectedIcon = Icons.Outlined.Home)
+            val eventsTab = TabBarItem(title = getString(R.string.bottom_navbar_event), selectedIcon = Icons.Filled.DateRange, unselectedIcon = Icons.Outlined.DateRange)
+            val historyTab = TabBarItem(title = getString(R.string.bottom_navbar_history), selectedIcon = Icons.Filled.List, unselectedIcon = Icons.Outlined.List)
 
             // creating a list of all the tabs
             val tabBarItems = listOf(homeTab, eventsTab, historyTab)
@@ -51,29 +57,62 @@ class MainActivity : ComponentActivity() {
             val navController = rememberNavController()
 
             ISENSmartCompanionTheme {
-                Scaffold(bottomBar = {TabView(tabBarItems, navController)}, modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    NavHost(navController = navController, startDestination = homeTab.title){
-                        composable(homeTab.title){
+                Scaffold( bottomBar = {
+                    TabView(tabBarItems, navController)
+                },
+                    modifier = Modifier.fillMaxSize()) { innerPadding ->
+
+                    NavHost(navController = navController, startDestination = homeTab.title) {
+                        composable(homeTab.title) {
                             MainScreen(innerPadding)
                         }
-                        composable(eventsTab.title){
+                        composable(eventsTab.title) {
+                            EventsScreen(
+                                innerPadding = innerPadding,
+                                eventHandler = { event ->
+                                    startEventDetailActivity(event)
+                                }
+                            )
                         }
-                        composable(historyTab.title){
-                            Text(historyTab.title)
+                        composable(historyTab.title) {
+                            HistoryScreen(innerPadding)
                         }
                     }
+
                 }
             }
         }
     }
-}
 
+    fun startEventDetailActivity(event: EventModel) {
+        val intent = Intent(this, EventDetailActivity::class.java).apply {
+            putExtra(EventDetailActivity.eventExtraKey, event)
+        }
+        startActivity(intent)
+    }
 
+    override fun onRestart() {
+        super.onRestart()
+        Log.d("lifecycle", "MainActivity onRestart")
+    }
 
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    ISENSmartCompanionTheme {
-        //MainScreen(PaddingValues)
+    override fun onResume() {
+        super.onResume()
+        Log.d("lifecycle", "MainActivity onResume")
+    }
+
+    override fun onStop() {
+        Log.d("lifecycle", "MainActivity onStop")
+        super.onStop()
+    }
+
+    override fun onPause() {
+        Log.d("lifecycle", "MainActivity onPause")
+        super.onPause()
+    }
+
+    override fun onDestroy() {
+        Log.d("lifecycle", "MainActivity onDestroy")
+        super.onDestroy()
     }
 }
---
+++ app/src/main/java/fr/isen/vincent/isensmartcompanion/screen/HistoryScreen.kt
@@ -0,0 +1,9 @@
+package fr.isen.vincent.isensmartcompanion.screen
+
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.runtime.Composable
+
+@Composable
+fun HistoryScreen(innerPadding : PaddingValues) {
+
+}
